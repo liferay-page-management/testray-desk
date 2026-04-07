01fe63e32584a8f271a4ebd899dd80548d94c0d2
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-04-07 10:45:00 +0100

    Add script to update case results daily

---
+++ src/scripts/update-case-results.ts
@@ -0,0 +1,36 @@
+import { getRoutineBuilds } from '@/services/build'
+import { getBuildCaseResults } from '@/services/case-result'
+
+import { inheritMetadata } from '@/lib/inherit-metadata'
+import { TEAMS } from '@/lib/teams'
+
+import { Routine } from '@/types/testray'
+
+async function updateCaseResults(routineId: Routine['id']) {
+	const [lastBuild, previousDayBuild] = await getRoutineBuilds({
+		routineId,
+		limit: 2,
+	})
+
+	const previousDayFailures = await getBuildCaseResults({
+		buildId: previousDayBuild.id,
+		statuses: ['FAILED'],
+	})
+
+	const caseResults = await getBuildCaseResults({
+		buildId: lastBuild.id,
+		statuses: ['FAILED', 'BLOCKED', 'UNTESTED'],
+	})
+
+	for (const caseResult of caseResults) {
+		await inheritMetadata(previousDayFailures, caseResult)
+	}
+}
+
+async function main() {
+	for (const team of Object.values(TEAMS)) {
+		updateCaseResults(team.routineId)
+	}
+}
+
+main()
